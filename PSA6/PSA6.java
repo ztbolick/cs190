165f36dd40b55ffb4169accd2440923b675ff2e2
Zac Bolick <ztbolick@gmail.com>
2018-04-02 12:24:43 -0700

    added background loop to copy and loop to copy mustang

--- PSA6/PSA6.java
+++ PSA6/PSA6.java
@@ -15,10 +15,28 @@ public class PSA6 {
   public static void main(String[] args) { 
 
 	// make a bunch of picture objects
-	Picture p1 = new Picture("/Users/ztbolick/Documents/Code/cs190/PSA6/resources/stang.jpg");
-	Picture p2 = new Picture("/Users/ztbolick/Documents/Code/cs190/PSA6/resources/pattern.jpg");
-	
-	
-	p1.explore();
+	Picture bg = new Picture("/Users/ztbolick/Documents/Code Project/cs190/PSA6/resources/landscape.jpg");
+	Picture stang = new Picture("/Users/ztbolick/Documents/Code Project/cs190/PSA6/resources/stang-sized.jpg"); 
+	Picture me = new Picture("/Users/ztbolick/Documents/Code Project/cs190/PSA6/resources/side-view-green1.jpg");
+	Picture collage = new Picture(1184, 501);
+
+	// Copy the Background
+	for (int y = 0; y < collage.getHeight(); y++) {
+		for (int x = 0; x < collage.getWidth(); x++) {
+			Pixel source = bg.getPixel (x, y); 
+			Pixel target = collage.getPixel (x, y);
+			target.setColor(source.getColor());	
+		}
+	}
+
+	for (int y = 280, y2 = 0; y2 < stang.getHeight(); y++, y2++) {
+		for (int x = 585, x2 = 0; x2 < stang.getWidth(); x++, x2++) {
+			Pixel source = stang.getPixel (x2, y2); 
+			Pixel target = collage.getPixel (x, y);
+			target.setColor(source.getColor());	
+		}
+	}	
+
+	collage.explore();
   }
 }
